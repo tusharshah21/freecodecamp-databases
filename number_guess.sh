cd888f57579337e5475f26076c68e378cf508adc
TUSHAR KUMAR SHAH <tusharkumarshah14394@gmail.com>
2026-02-14 21:51:59 -0500

    feat: add range validation for guess input

--- number_guess.sh
+++ number_guess.sh
@@ -53,6 +53,12 @@ while true; do
     continue
   fi
   
+  # Validate guess is within the valid range
+  if [[ $GUESS -lt 1 ]] || [[ $GUESS -gt 1000 ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+  
   ((NUMBER_OF_GUESSES++))
   
   # Compare guess with secret number
